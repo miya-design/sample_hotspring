000bf1109c4c83b2f47c4228cd4c0f8db5662f72
miya-design <86173270+miya-design@users.noreply.github.com>
2021-07-12 16:56:03 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(function(){
+    $('#rec-slick').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed:5000,
+        arrows:false,
+        centerPadding:"100px",
+        centerMode:true,
+          responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerPadding:0,
+
+            }
+          }
+        ]
+      });
+});
+$(function(){
+  $('#nav-btn').on('click',function(){
+    $(this).toggleClass('-active');
+    $('#nav').toggleClass('-active');
+  })
+
+});
+
+$(function(){
+  $('.js-accordion').on('click',function(){
+    //$(this).toggleClass('-active');//
+    $(this).next().slideToggle();
+  })
+});
+
+$(function(){
+  new WOW().init();
+
+});
